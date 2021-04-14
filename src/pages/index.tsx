41d3a9c534bf13210344504b7a0a83314808a795
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-14 08:37:26 -0300

    Component Input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+import { Flex, Button, Stack} from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
 
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex
       w="100vw"
@@ -20,40 +21,11 @@ export default function Home() {
         {/* O Stack faz uma pilha, e atribui os estilos para todos que estiverem envoltos nele, nesse caso estamos apenas colocando um espaço entre os elementos, como se fosse o wrap do flex */}
         <Stack spacing="4">
 
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-            <Input
-              name="email"
-              id="email"
-              type="email"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-          </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="password">Senha</FormLabel>
-            <Input
-              name="password"
-              id="password"
-              type="password" 
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-          </FormControl>
+          <Input name="email" label="E-mail" type="email" />
+          <Input name="password" label="Senha" type="password" />
       
         </Stack>
-        
+
         <Button type="submit" mt="6" colorScheme="pink">
           Entrar
         </Button>
