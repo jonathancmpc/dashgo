d631cb5174a28dbba67513ab26117a9230db32a4
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-14 09:59:21 -0300

    Component Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,19 @@
-import { Head } from "next/document";
+import { Flex } from "@chakra-ui/layout";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
   return (
-    <>
+    <Flex direction="column" h="100vh">
       <Header />
-    </>
+
+      <Flex
+        w="100%"
+        my="6"
+        maxWidth={1480}
+      >
+        <Sidebar />
+      </Flex>
+    </Flex>
   )
 }
