a6691caa8d8c24f23eb4fa19fd3e8920133e6b79
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-14 09:01:28 -0300

    Header component initial code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
+import { Head } from 'next/document';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Head } from "next/document";
+import { Header } from "../components/Header";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
