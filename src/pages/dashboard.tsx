100e2b656815b3aa927e4dd8589c4b04cf4896bf
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-14 10:58:11 -0300

    First Chart in Dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,18 +1,88 @@
-import { Flex } from "@chakra-ui/layout";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 
+// Para que o apexcharts funcione no Next, é necessário passar a função dynamic, que renderiza do lado do cliente dinamicamente, por isso passamos o ssr false, pois assim irá carregar somente do lado do browser e não do lado do servidor(exigência do apexchart).
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+})
+
+const options: any = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500]
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600]
+    },
+    axisTicks: {
+      color: theme.colors.gray[600]
+    },
+    categories: [
+      '2021-03-18T00:00:00.000Z',
+      '2021-03-19T00:00:00.000Z',
+      '2021-03-20T00:00:00.000Z',
+      '2021-03-21T00:00:00.000Z',
+      '2021-03-22T00:00:00.000Z',
+      '2021-03-23T00:00:00.000Z',
+      '2021-03-24T00:00:00.000Z',
+    ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityForm: 0.7,
+      opacityTo: 0.3
+    }
+  },
+};
+
+const series = [
+  { 
+    name: 'series1', 
+    data:  [31,120,10,28,61,18,109]
+  }
+];
+
 export default function Dashboard() {
+
   return (
     <Flex direction="column" h="100vh">
       <Header />
 
-      <Flex
-        w="100%"
-        my="6"
-        maxWidth={1480}
-      >
+      <Flex w="100%" my="6" maxWidth={1480}>
         <Sidebar />
+
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex=start">
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">Inscritos da semana</Text>
+            <Chart options={options} series={series} type="area" height={160} />
+          </Box>
+
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+            <Chart options={options} series={series} type="area" height={160} />
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   )
