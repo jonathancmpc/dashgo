155e670d9309a65b0e7908bf4353713eb35e47c5
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-15 09:36:15 -0300

    Close sidebar if change router

--- src/contexts/SidebarDrawerContext.tsx
+++ src/contexts/SidebarDrawerContext.tsx
@@ -1,5 +1,6 @@
 import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useEffect } from "react";
+import { useRouter } from "next/dist/client/router";
 
 interface SidebarDrawerProviderProps {
   children: ReactNode;
@@ -11,6 +12,12 @@ const SidebarDrawerContext = createContext({} as SidebarDrawerContextData);
 
 export function SidebarDrawerProvider({ children }: SidebarDrawerProviderProps) {
   const disclosure = useDisclosure(); //Hook do chakra para abrir ou fechar a sidebar, nele vem os métodos isOpen e onOpen e onClose
+  const router = useRouter(); //Pegando a informação da rota
+
+  //Toda vez que a rota mudar, ou seja, toda vez que clicarmos em um link da sidebar, é para fechá-la
+  useEffect(() => {
+    disclosure.onClose();
+  }, [router.asPath])
 
   return (
     <SidebarDrawerContext.Provider value={disclosure}>
